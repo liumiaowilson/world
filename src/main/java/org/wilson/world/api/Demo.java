a3e910faaa538338272e09f1b9b5b227e36b986c
liumiaowilson <lockleeliumiaowilson@gmail.com>
2016-07-01 17:54:36 +0800

    add demo update for log4j

--- src/main/java/org/wilson/world/api/Demo.java
+++ src/main/java/org/wilson/world/api/Demo.java
@@ -4,12 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
+
 @Path("/demo")
 public class Demo {
+    private Logger logger = Logger.getLogger(Demo.class);
+    
     @GET
     @Path("/msg")
     public Response message() {
         String msg = "Hello World";
+        if(logger.isDebugEnabled()) {
+            logger.debug("print demo message");
+        }
         return Response.status(200).entity(msg).build();
     }
 }
