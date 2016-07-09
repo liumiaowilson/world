fd2304098a3b42f6fec2c9d6c461b7926eafeffc
liumiaowilson <lockleeliumiaowilson@gmail.com>
2016-07-09 12:59:35 +0800

    improve manager loading

--- src/main/java/org/wilson/world/manager/ManagerLoader.java
+++ src/main/java/org/wilson/world/manager/ManagerLoader.java
@@ -1,5 +1,9 @@
 package org.wilson.world.manager;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -8,26 +12,45 @@ import org.apache.log4j.Logger;
 public class ManagerLoader implements ServletContextListener {
     private static final Logger logger = Logger.getLogger(ManagerLoader.class);
     
+    @SuppressWarnings("rawtypes")
+    private static List<Class> managerClazzes = new ArrayList<Class>();
+    static {
+        loadManagerClazz();
+    }
+    
+    private static void loadManagerClazz() {
+        managerClazzes.add(CacheManager.class);
+        managerClazzes.add(ConfigManager.class);
+        managerClazzes.add(ConsoleManager.class);
+        managerClazzes.add(DataManager.class);
+        managerClazzes.add(EventManager.class);
+        managerClazzes.add(IdeaManager.class);
+        managerClazzes.add(ItemManager.class);
+        managerClazzes.add(MarkManager.class);
+        managerClazzes.add(SecManager.class);
+        managerClazzes.add(UserManager.class);
+    }
+    
     @Override
     public void contextDestroyed(ServletContextEvent event) {
         logger.info("Manager loader context destroyed.");
     }
 
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
     public void contextInitialized(ServletContextEvent event) {
         logger.info("Manager loader context initialized.");
         
-        EventManager.getInstance();
-        ConfigManager.getInstance();
-        ConsoleManager.getInstance();
-        SecManager.getInstance();
-        
-        ItemManager.getInstance();
-        CacheManager.getInstance();
-        MarkManager.getInstance();
-        UserManager.getInstance();
-        IdeaManager.getInstance();
-        DataManager.getInstance();
+        for(Class clazz : managerClazzes) {
+            try {
+                Method getInstanceMethod = clazz.getDeclaredMethod("getInstance");
+                getInstanceMethod.invoke(null);
+                logger.info("Instantiated class [" + clazz.getCanonicalName() + "]");
+            }
+            catch(Exception e) {
+                logger.error("failed to instantiate manager class", e);
+            }
+        }
         
         if(ConfigManager.getInstance().isPreloadOnStartup()) {
             CacheManager.getInstance().doPreload();
