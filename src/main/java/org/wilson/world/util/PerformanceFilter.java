02c4094ffc26bcc252fbe34ed3ddf582d0eda8e6
liumiaowilson <lockleeliumiaowilson@gmail.com>
2016-07-10 16:23:27 +0800

    add performance filter

---
+++ src/main/java/org/wilson/world/util/PerformanceFilter.java
@@ -0,0 +1,42 @@
+package org.wilson.world.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+public class PerformanceFilter implements Filter {
+    private static final Logger logger = Logger.getLogger(PerformanceFilter.class);
+    
+    @Override
+    public void destroy() {
+        logger.info("Performance filter destroyed.");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        long start = System.currentTimeMillis();
+        chain.doFilter(req, resp);
+        long end = System.currentTimeMillis();
+        long elapsed = end - start;
+        String name = "servlet";
+        if(req instanceof HttpServletRequest) {
+            name = ((HttpServletRequest)req).getRequestURI();
+        }
+        logger.info(name + " took " + elapsed + " ms.");
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        logger.info("Performance filter initiated.");
+    }
+
+}
