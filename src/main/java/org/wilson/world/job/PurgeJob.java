6c72c959eb51ca030bc59b659108bf8ff26e9218
liumiaowilson <lockleeliumiaowilson@gmail.com>
2016-07-14 14:17:48 +0800

    add purge framework

---
+++ src/main/java/org/wilson/world/job/PurgeJob.java
@@ -0,0 +1,41 @@
+package org.wilson.world.job;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+import org.wilson.world.db.DBUtils;
+import org.wilson.world.manager.ConfigManager;
+import org.wilson.world.schedule.DefaultJob;
+
+public abstract class PurgeJob extends DefaultJob {
+    private static final Logger logger = Logger.getLogger(PurgeJob.class);
+    
+    public abstract PreparedStatement getPurgeStatement(Connection con) throws SQLException;
+    
+    @Override
+    public void execute() {
+        if(!ConfigManager.getInstance().isInMemoryMode()) {
+            Connection con = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try {
+                con = DBUtils.getConnection();
+                ps = this.getPurgeStatement(con);
+                int count = ps.executeUpdate();
+                logger.info("Purged " + count + " row(s) by [" + this.getJobName() + "].");
+            }
+            catch(Exception e) {
+                logger.error("failed to run " + this.getJobName(), e);
+            }
+            finally {
+                DBUtils.closeQuietly(con, ps, rs);
+            }
+        }
+        else {
+            logger.info(this.getJobName() + " is not run in memory mode.");
+        }
+    }
+}
--- src/main/java/org/wilson/world/stats/PurgeStatsJob.java
+++ src/main/java/org/wilson/world/stats/PurgeStatsJob.java
@@ -2,41 +2,18 @@ package org.wilson.world.stats;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import org.apache.log4j.Logger;
-import org.wilson.world.db.DBUtils;
-import org.wilson.world.manager.ConfigManager;
-import org.wilson.world.schedule.DefaultJob;
+import org.wilson.world.job.PurgeJob;
 
-public class PurgeStatsJob extends DefaultJob {
-    private static final Logger logger = Logger.getLogger(PurgeStatsJob.class);
-    
+public class PurgeStatsJob extends PurgeJob {
     @Override
-    public void execute() {
-        if(!ConfigManager.getInstance().isInMemoryMode()) {
-            Connection con = null;
-            PreparedStatement ps = null;
-            ResultSet rs = null;
-            try {
-                con = DBUtils.getConnection();
-                long current = System.currentTimeMillis();
-                long last = current - 30 * 24 * 60 * 60 * 1000L;
-                String sql = "delete from stats where time < ?";
-                ps = con.prepareStatement(sql);
-                ps.setLong(1, last);
-                int count = ps.executeUpdate();
-                logger.info("Purged " + count + " row(s) from stats.");
-            }
-            catch(Exception e) {
-                logger.error("failed to purge stats", e);
-            }
-            finally {
-                DBUtils.closeQuietly(con, ps, rs);
-            }
-        }
-        else {
-            logger.info("Purge stats job is not run in memory mode.");
-        }
+    public PreparedStatement getPurgeStatement(Connection con) throws SQLException {
+        long current = System.currentTimeMillis();
+        long last = current - 30 * 24 * 60 * 60 * 1000L;
+        String sql = "delete from stats where time < ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setLong(1, last);
+        return ps;
     }
 }
