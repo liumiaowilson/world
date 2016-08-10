9ea759bad8d9f78a6d9e6c615dd5888d6b566f5f
Liu Miao <mialiu@ebay.com>
2016-08-10 10:36:00 +0800

    add demo for some sites

--- src/test/org/wilson/world/util/JSoupTest.java
+++ src/test/org/wilson/world/util/JSoupTest.java
@@ -1,6 +1,8 @@
 package org.wilson.world.util;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -144,5 +146,106 @@ public class JSoupTest {
         }
         
         System.out.println(sb.toString());
+    }
+    
+    @Test
+    public void testPornPic() throws Exception {
+        Document doc = Jsoup.connect("http://www.pornpics.com/recent/hardcore/").get();
+        Elements elements = doc.select("div#main li.thumbwook");
+        for(int i = 0; i < elements.size(); i++) {
+            Element li = elements.get(i);
+            Elements item = li.select("a.rel-link");
+            String title = item.attr("title");
+            String url = item.attr("href");
+            
+            System.out.println(title);
+            System.out.println(url);
+        }
+    }
+    
+    @Test
+    public void testPornPicFetch() throws Exception {
+        Document doc = Jsoup.connect("http://www.pornpics.com/galleries/european-teen-sheri-vi-taking-hardcore-penetration-of-shaved-cunt-and-anus/").get();
+        Elements elements = doc.select("div#main li.thumbwook");
+        for(int i = 0; i < elements.size(); i++) {
+            Element li = elements.get(i);
+            Elements item = li.select("a.rel-link");
+            String url = item.attr("href");
+            
+            System.out.println(url);
+        }
+    }
+    
+    @Test
+    public void testBeauty() throws Exception {
+        Document doc = Jsoup.connect("http://www.mm131.com/xinggan/").get();
+        Elements elements = doc.select("div.main dl.list-left dd");
+        for(int i = 0; i < elements.size(); i++) {
+            Elements item = elements.get(i).select("a");
+            if(item.size() == 1) {
+                String url = item.attr("href");
+                
+                System.out.println(url);
+            }
+        }
+    }
+    
+    @Test
+    public void testBeautyEach() throws Exception {
+        Document doc = Jsoup.connect("http://www.mm131.com/xinggan/2606.html").get();
+        Elements elements = doc.select("div.content div.content-page span.page-ch");
+        String text = elements.get(0).text();
+        
+        Pattern p = Pattern.compile("([0-9]+)");
+        Matcher m = p.matcher(text);
+        if(m.find()) {
+            int pages = Integer.parseInt(m.group(1));
+            for(int i = 1; i <= pages; i++) {
+                System.out.println("http://img1.mm131.com/pic/2606/" + i + ".jpg");
+            }
+        }
+    }
+    
+    @Test
+    public void testHentai() throws Exception {
+        ConfigManager.getInstance();
+        Document doc = Jsoup.connect("http://g.e-hentai.org/").get();
+        Elements elements = doc.select("div.ido table.itg div.it5 a");
+        for(int i = 0; i < elements.size(); i++) {
+            String url = elements.get(i).attr("href");
+            
+            System.out.println(url);
+        }
+    }
+    
+    @Test
+    public void testHentaiEach() throws Exception {
+        ConfigManager.getInstance();
+        Document doc = Jsoup.connect("http://g.e-hentai.org/g/963653/75852cb933/").get();
+        Elements elements = doc.select("div.gtb p");
+        String text = elements.text();
+        
+        Pattern p = Pattern.compile("of ([0-9]+) image");
+        Matcher m = p.matcher(text);
+        if(m.find()) {
+            int images = Integer.parseInt(m.group(1));
+            System.out.println(images);
+        }
+        
+        elements = doc.select("div.gdtm a");
+        for(int i = 0; i < elements.size(); i++) {
+            String url = elements.get(i).attr("href");
+            
+            System.out.println(url);
+        }
+    }
+    
+    @Test
+    public void testHentaiGet() throws Exception {
+        ConfigManager.getInstance();
+        Document doc = Jsoup.connect("http://g.e-hentai.org/s/29ff204432/963653-1").get();
+        Elements elements = doc.select("div.sni>a img");
+        String url = elements.attr("src");
+        System.out.println(url);
     }
 }
