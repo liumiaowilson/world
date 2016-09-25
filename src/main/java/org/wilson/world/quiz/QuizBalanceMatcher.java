a0d41098b1099f59fc7a3fe09d6a74cfd6fac0fb
Liu Miao <mialiu@ebay.com>
2016-09-25 14:47:12 +0800

    fix quiz balance matcher issue

--- src/main/java/org/wilson/world/quiz/QuizBalanceMatcher.java
+++ src/main/java/org/wilson/world/quiz/QuizBalanceMatcher.java
@@ -1,5 +1,8 @@
 package org.wilson.world.quiz;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
 import org.wilson.world.balance.DefaultBalanceMatcher;
 import org.wilson.world.manager.QuizDataManager;
 import org.wilson.world.menu.MenuItem;
@@ -7,6 +10,15 @@ import org.wilson.world.menu.MenuItem;
 public abstract class QuizBalanceMatcher extends DefaultBalanceMatcher {
     @SuppressWarnings("rawtypes")
     public abstract Class getQuizClass();
+    
+    protected boolean match(HttpServletRequest request, String uri) {
+        if(StringUtils.isBlank(uri)) {
+            return false;
+        }
+        
+        String str = request.getRequestURI() + "?" + request.getQueryString();
+        return str.startsWith(uri) && !str.contains("action=next");
+    }
     
     protected String getQuizPaperPage() {
         return "/jsp/quiz_paper.jsp";
