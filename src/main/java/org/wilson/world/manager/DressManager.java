882a83e13a1e9fa3f938b4b2d96bb1eb97fc5c11
Liu Miao <mialiu@ebay.com>
2016-09-26 20:25:17 +0800

    add more quiz to dress

--- src/main/java/org/wilson/world/manager/DressManager.java
+++ src/main/java/org/wilson/world/manager/DressManager.java
@@ -3,9 +3,11 @@ package org.wilson.world.manager;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 import org.wilson.world.dress.DressColor;
@@ -201,6 +203,17 @@ public class DressManager {
         this.pairs = new ArrayList<List<QuizPair>>();
         
         this.pairs.add(this.getEnergyKeywordsQuizPairs());
+        this.pairs.add(this.getEnergyLineQuizPairs());
+        this.pairs.add(this.getEnergyEnergyQuizPairs());
+        this.pairs.add(this.getEnergyColorQuizPairs());
+        this.pairs.add(this.getEnergyTextureQuizPairs());
+        this.pairs.add(this.getEnergyContrastQuizPairs());
+        this.pairs.add(this.getEnergyColorsQuizPairs());
+        this.pairs.add(this.getEnergySizeQuizPairs());
+        this.pairs.add(this.getFamilyNameQuizPairs());
+        this.pairs.add(this.getFamilyTypeQuizPairs());
+        this.pairs.add(this.getColorNameQuizPairs());
+        this.pairs.add(this.getHomeBaserNameQuizPairs());
     }
     
     private int getNumOfQuizPairs() {
@@ -221,6 +234,263 @@ public class DressManager {
             pair.top = "Key words for [" + energy.name + "]";
             pair.bottom = energy.getKeywords();
             pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getEnergyLineQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(SeasonEnergy energy : this.energies.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Line for [" + energy.name + "]";
+            pair.bottom = energy.line;
+            pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getEnergyEnergyQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(SeasonEnergy energy : this.energies.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Energy for [" + energy.name + "]";
+            pair.bottom = energy.energy;
+            pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getEnergyColorQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(SeasonEnergy energy : this.energies.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Color for [" + energy.name + "]";
+            pair.bottom = energy.color;
+            pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getEnergyTextureQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(SeasonEnergy energy : this.energies.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Texture for [" + energy.name + "]";
+            pair.bottom = energy.texture;
+            pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getEnergyContrastQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(SeasonEnergy energy : this.energies.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Contrast for [" + energy.name + "]";
+            pair.bottom = energy.contrast;
+            pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getEnergyColorsQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(SeasonEnergy energy : this.energies.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Colors for [" + energy.name + "]";
+            pair.bottom = energy.colors;
+            pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getEnergySizeQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(SeasonEnergy energy : this.energies.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Size for [" + energy.name + "]";
+            pair.bottom = energy.size;
+            pair.url = "javascript:jumpTo('season_energy_edit.jsp?id=" + energy.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private void addToDressColors(Map<String, DressColor> data, DressColorFamily family, String name) {
+        String colorName = null;
+        if("Spring".equals(name)) {
+            colorName = family.spring;
+        }
+        else if("Summer".equals(name)) {
+            colorName = family.summer;
+        }
+        else if("Autumn".equals(name)) {
+            colorName = family.autumn;
+        }
+        else if("Winter".equals(name)) {
+            colorName = family.winter;
+        }
+        else if("Clear".equals(name)) {
+            colorName = family.clear;
+        }
+        else if("Soft".equals(name)) {
+            colorName = family.soft;
+        }
+        else if("IcyWarm".equals(name)) {
+            colorName = family.icyWarm;
+        }
+        else if("IcyCool".equals(name)) {
+            colorName = family.icyCool;
+        }
+        
+        if(colorName == null) {
+            return;
+        }
+        
+        DressColor color = this.getDressColor(colorName);
+        if(color == null) {
+            return;
+        }
+        
+        data.put(name, color);
+    }
+    
+    public Map<String, DressColor> getDressColors(DressColorFamily family) {
+        if(family == null) {
+            return Collections.emptyMap();
+        }
+        
+        Map<String, DressColor> ret = new HashMap<String, DressColor>();
+        
+        this.addToDressColors(ret, family, "Spring");
+        this.addToDressColors(ret, family, "Summer");
+        this.addToDressColors(ret, family, "Autumn");
+        this.addToDressColors(ret, family, "Winter");
+        this.addToDressColors(ret, family, "Clear");
+        this.addToDressColors(ret, family, "Soft");
+        this.addToDressColors(ret, family, "IcyWarm");
+        this.addToDressColors(ret, family, "IcyCool");
+        
+        return ret;
+    }
+    
+    private List<QuizPair> getFamilyNameQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(DressColorFamily family : this.families.values()) {
+            Map<String, DressColor> data = this.getDressColors(family);
+            for(DressColor color : data.values()) {
+                QuizPair pair = new QuizPair();
+                pair.id = id++;
+                pair.top = "Family name of [" + color.name + "]<div style='background-color:" + color.color + ";width:100%;height:20px'/>";
+                pair.bottom = family.name;
+                pair.url = "javascript:jumpTo('dress_color_family_edit.jsp?id=" + family.id + "')";
+                
+                pairs.add(pair);
+            }
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getFamilyTypeQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(DressColorFamily family : this.families.values()) {
+            Map<String, DressColor> data = this.getDressColors(family);
+            for(Entry<String, DressColor> entry : data.entrySet()) {
+                String type = entry.getKey();
+                DressColor color = entry.getValue();
+                QuizPair pair = new QuizPair();
+                pair.id = id++;
+                pair.top = "Family type of [" + color.name + "]<div style='background-color:" + color.color + ";width:100%;height:20px'/>";
+                pair.bottom = type;
+                pair.url = "javascript:jumpTo('dress_color_family_edit.jsp?id=" + family.id + "')";
+                
+                pairs.add(pair);
+            }
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getColorNameQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(DressColor color : this.colors.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            pair.top = "Name of the color<div style='background-color:" + color.color + ";width:100%;height:20px'/>";
+            pair.bottom = color.name;
+            pair.url = "javascript:jumpTo('dress_color_edit.jsp?id=" + color.id + "')";
+            
+            pairs.add(pair);
+        }
+        
+        return pairs;
+    }
+    
+    private List<QuizPair> getHomeBaserNameQuizPairs() {
+        List<QuizPair> pairs = new ArrayList<QuizPair>();
+        
+        int id = this.getNumOfQuizPairs() + 1;
+        for(HomeBase homebase : this.homebases.values()) {
+            QuizPair pair = new QuizPair();
+            pair.id = id++;
+            StringBuilder sb = new StringBuilder();
+            for(String item : homebase.items) {
+                sb.append("<p>" + item + "</p>");
+            }
+            pair.top = sb.toString();
+            pair.bottom = homebase.name;
+            pair.url = "javascript:jumpTo('homebase_edit.jsp?id=" + homebase.id + "')";
             
             pairs.add(pair);
         }
