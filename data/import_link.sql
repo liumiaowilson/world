01461d96070abbaab3a562a15ee18f6a46572ed4
Liu Miao <mialiu@ebay.com>
2016-10-25 09:47:03 +0800

    alter links table

--- data/import_link.sql
+++ data/import_link.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `links` (
   `label` varchar(20) NOT NULL,
   `item_type` varchar(20) NOT NULL,
   `item_id` int(11) NOT NULL,
-  `menu_id` varchar(50) NOT NULL,
+  `menu_id` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
