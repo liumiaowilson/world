0db554847720ef44db7a87d2582e9a03969c68be
Liu Miao <mialiu@ebay.com>
2016-11-25 20:51:23 +0800

    add equal to bookmark obj

--- src/main/java/org/wilson/world/model/Bookmark.java
+++ src/main/java/org/wilson/world/model/Bookmark.java
@@ -10,4 +10,26 @@ public class Bookmark {
     public String content;
     
     public String url;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bookmark other = (Bookmark) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
