ae0aed88ba49ba652d63b6c04681b6d36aa83f20
Miao (Wilson) Liu <wilson@clicktocloud.com>
2017-07-29 08:41:32 +1000

    fix compilation issue

--- src/main/java/org/wilson/world/api/NotesAPI.java
+++ src/main/java/org/wilson/world/api/NotesAPI.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
